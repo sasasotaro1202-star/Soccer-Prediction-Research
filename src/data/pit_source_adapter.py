81914ecee5a2920fa5260fce43fc49978cd506a4
sasasotaro1202-star <sasa.sotaro1202@icloud.com>
2026-09-12 12:50:24 +0900

    Fix PIT adapter compatibility export for replay tests

--- src/data/pit_source_adapter.py
+++ src/data/pit_source_adapter.py
@@ -1,3 +1,4 @@
-"""Compatibility wrapper for the time-precise PIT archive adapter."""
+"""Compatibility exports for the time-precise PIT archive adapter."""
 
 from src.data.pit_source_adapter_v2 import *
+from src.data.pit_source_adapter_v2 import _result_lower_bound
