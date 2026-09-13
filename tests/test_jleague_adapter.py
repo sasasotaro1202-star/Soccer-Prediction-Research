57247a39e93747f2155cae321f9529e840dd8e3d
sasasotaro1202-star <sasa.sotaro1202@icloud.com>
2026-09-13 15:18:04 +0900

    Test Japanese J.League label normalization

--- tests/test_jleague_adapter.py
+++ tests/test_jleague_adapter.py
@@ -1,11 +1,13 @@
-import pandas as pd
-
 from src.data.jleague_adapter import _norm_comp, TARGETS
 
 
 def test_jleague_competition_normalization_is_conservative():
     assert _norm_comp("J1") == "J1"
     assert _norm_comp("J1 League") == "J1"
+    assert _norm_comp("Ｊ１") == "J1"
+    assert _norm_comp("Ｊリーグ ディビジョン１") == "J1"
+    assert _norm_comp("明治安田Ｊ２リーグ") == "J2"
+    assert _norm_comp("Ｊ３リーグ") == "J3"
     assert _norm_comp("J2") == "J2"
     assert _norm_comp("J3 League") == "J3"
     assert _norm_comp("Emperor Cup") is None
